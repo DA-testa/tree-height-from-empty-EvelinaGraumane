b285324b434b5d7e5c7bcc73fa5d93c5d711f611
EvelinaGraumane <125298141+EvelinaGraumane@users.noreply.github.com>
2023-02-28 17:31:44 +0000

    test3

--- tree_height.py
+++ tree_height.py
@@ -5,10 +5,9 @@ import threading
 import numpy
 import os
 
+
 def compute_height(n, parents):
-    # Write this function
     max_height = 0
-    # Your code here
     height = [0] * n
     for up in range(n):
         distance = get_distance(up, parents, height)
@@ -16,24 +15,22 @@ def compute_height(n, parents):
             max_height = distance
     return max_height
 
-def get_distance(up, parents, height):
 
+def get_distance(up, parents, height):
     if height[up] != 0:
         return height[up]
     if parents[up] == -1:
         height[up] = 1
     else:
         height[up] = get_distance(parents[up], parents, height) + 1
-        return height[up]
+    return height[up]
+
 
 def main():
-    # implement input form keyboard and from files
-    text = input().strip()
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
+    text = input().strip().upper()
     if text == 'I':
         n = int(input("Elementu skaits: "))
-        parents = numpy.asarray(list(map(int,input("Vertibas: ").strip().split())))
+        parents = list(map(int, input("Vertibas: ").strip().split()))
     elif text == 'F':
         name = input("Faila nosaukums: ")
         if 'a' in name:
@@ -42,23 +39,15 @@ def main():
         file_path = os.path.join(os.getcwd(), 'test', name)
         with open(file_path, 'r') as f:
             n = int(f.readline().strip())
-            parents = numpy.asarray(list(map(int, f.readline().strip().split())))
+            parents = list(map(int, f.readline().strip().split()))
     else:
         print("Nepareiza ievade")
         return
 
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
     max_height = compute_height(n, parents)
     print(max_height)
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
+
+sys.setrecursionlimit(10**7)
+threading.stack_size(2**27)
 threading.Thread(target=main).start()
-main()
-# print(numpy.array([1,2,3]))
